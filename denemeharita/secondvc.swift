0a959bfd8594561cd04e3d6d055719eb444c7d03
erdemoden <erdemoden5@gmail.com>
2021-05-27 03:58:54 +0300

    first commit

---
+++ denemeharita/secondvc.swift
@@ -0,0 +1,29 @@
+//
+//  secondvc.swift
+//  denemeharita
+//
+//  Created by erdem öden on 26.05.2021.
+//
+
+import UIKit
+
+class secondvc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
